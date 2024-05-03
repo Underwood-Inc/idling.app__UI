b79831c0b2b557855dce5d4a9f58f34866b6201b
Michael C. Seaward <m.seaward@mamok.in>
2024-05-03 18:15:37 -0300

    feat: add discord embed to main page (#54)
    Co-authored-by: Michael Seaward <m.seaward@pm.me>

---
+++ src/app/components/discord-embed/DiscordEmbed.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+export const DiscordEmbed: React.FC = () => {
+  return (
+    <iframe
+      src="https://discord.com/widget?id=1234783462335189080&theme=dark"
+      width="350"
+      height="500"
+      // @ts-expect-error
+      allowtransparency="true"
+      frameBorder="0"
+      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+    ></iframe>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { About } from "./components/about/About";
 import { Card } from "./components/card/Card";
+import { DiscordEmbed } from "./components/discord-embed/DiscordEmbed";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h3>About</h3>
+      <div className={styles.content}>
+        <Card>
+          <h3>About</h3>
+          <About />
+        </Card>
 
-      <Card>
-        <About />
-      </Card>
+        <DiscordEmbed />
+      </div>
     </main>
   );
 }
