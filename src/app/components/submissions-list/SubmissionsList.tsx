495c834b1b9ee85725bed05e327e02a8e3e3c912
underwood_inc <m.seaward@pm.me>
2024-07-25 11:02:55 -0300

    feat: add noStore to prevent postgres response caching

--- src/app/components/submissions-list/SubmissionsList.tsx
+++ src/app/components/submissions-list/SubmissionsList.tsx
@@ -1,4 +1,7 @@
+import { unstable_noStore as noStore } from "next/cache";
+import { Suspense } from "react";
 import sql from "../../../lib/db";
+import Loading from "../home-avatar/loader";
 import { DeleteSubmissionForm } from "../submission-forms/delete-submission-form/DeleteSubmissionForm";
 import "./SubmissionsList.css";
 
@@ -9,16 +12,19 @@ interface Submission {
 }
 
 export default async function SubmissionsList() {
+  noStore();
   const submissions: Submission[] = await sql`SELECT * FROM submissions`;
 
   return (
     <ol className="submission_list">
-      {submissions.map(({ submission_id, submission_name }) => (
-        <li key={submission_id} className="submission_wrapper">
-          <span>{submission_name}</span>
-          <DeleteSubmissionForm id={submission_id} name={submission_name} />
-        </li>
-      ))}
+      <Suspense fallback={<Loading />}>
+        {submissions.map(({ submission_id, submission_name }) => (
+          <li key={submission_id} className="submission_wrapper">
+            <span>{submission_name}</span>
+            <DeleteSubmissionForm id={submission_id} name={submission_name} />
+          </li>
+        ))}
+      </Suspense>
     </ol>
   );
 }
