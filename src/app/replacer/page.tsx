f080ecaee9269b9d9b7cd1c376504aa2e7007fd4
underwood_inc <m.seaward@pm.me>
2024-07-25 10:47:15 -0300

    feat: add suspense wrapper to submissions list

--- src/app/replacer/page.tsx
+++ src/app/replacer/page.tsx
@@ -1,37 +1,17 @@
-import sql from "../../lib/db";
+import { Suspense } from "react";
+import Loading from "../components/home-avatar/loader";
 import { AddSubmissionForm } from "../components/submission-forms/add-submission-form/AddSubmissionForm";
-import { DeleteSubmissionForm } from "../components/submission-forms/delete-submission-form/DeleteSubmissionForm";
+import SubmissionsList from "../components/submissions-list/SubmissionsList";
 import rootStyles from "../page.module.css";
-import styles from "./page.module.css";
-
-interface Submission {
-  submission_id: number;
-  submission_name: string;
-  submission_datetime: string;
-}
 
 export default async function Replacer() {
-  const submissions: Submission[] = await sql`SELECT * FROM submissions`;
-
   return (
     <main className={rootStyles.main}>
       <h3>submissions</h3>
 
-      <div className={rootStyles.content}>
-        <ol>
-          {submissions.map(({ submission_id, submission_name }) => (
-            <li key={submission_id}>
-              <div className={styles.submission}>
-                <span>{submission_name}</span>
-                <DeleteSubmissionForm
-                  id={submission_id}
-                  name={submission_name}
-                />
-              </div>
-            </li>
-          ))}
-        </ol>
-      </div>
+      <Suspense fallback={<Loading />}>
+        <SubmissionsList />
+      </Suspense>
 
       <AddSubmissionForm />
     </main>
