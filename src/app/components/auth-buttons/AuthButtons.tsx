f6e470f547fd7c7bef66e7389d43dabe3b7174e9
underwood_inc <m.seaward@pm.me>
2024-07-27 12:56:59 -0300

    feat: add signin redirect to referer

--- src/app/components/auth-buttons/AuthButtons.tsx
+++ src/app/components/auth-buttons/AuthButtons.tsx
@@ -1,11 +1,15 @@
+import { headers } from 'next/headers';
 import { signIn, signOut } from '../../../../auth';
 
 export function SignIn() {
+  const headersList = headers();
+  const referer = headersList.get('referer');
+
   return (
     <form
       action={async () => {
         'use server';
-        await signIn('twitch');
+        await signIn('twitch', { redirectTo: referer || '/' });
       }}
     >
       <button type="submit">Login with Twitch</button>
