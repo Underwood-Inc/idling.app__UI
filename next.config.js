4e03ef60cfcac015f53709ba4f792fabf571e9f0
underwood_inc <m.seaward@pm.me>
2024-07-29 07:46:02 -0300

    feat: breaking changes add nextauth adapter for postgres integration
    - obscure and useless errors exist: something to do with cloudflare and nextauth
    - [TypeError: CloudflareSocket is not a constructor]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  // webpack5: true,
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // if (!isServer) {
+    //   config.node = {
+    //     fs: 'empty'
+    //   };
+    // }
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      stream: false,
+      dns: false,
+      net: false,
+      'pg-native': false,
+      crypto: false,
+      tls: false
+    };
+
+    return config;
+  }
+  // experimental: {
+  //   serverComponentsExternalPackages: ['pg']
+  // }
 };
 
 module.exports = nextConfig;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import twitch from 'next-auth/providers/twitch';
+import { Pool } from 'pg';
+// import { Pool } from '@neondatabase/serverless';
+import PostgresAdapter from './adapter';
+// const { Client } = require('pg');
 
 interface User {
   id: string;
@@ -9,44 +13,55 @@ interface User {
   created_date: string;
 }
 
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  port: 5432 || 443
+});
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [twitch],
-  // adapter: {},
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      // breaks here, try adapter
-      // https://www.jakobmaier.at/posts/next-auth-postgres-adapter/
-      // let sql = postgres(process.env.PGSQL_HOST!, {
-      //   ssl: 'allow'
-      // });
-      const isAllowedToSignIn = true;
+  adapter: PostgresAdapter(pool)
+  // callbacks: {
+  //   async signIn({ user, account, profile, email, credentials }) {
+  //     // breaks here, try adapter
+  //     // https://www.jakobmaier.at/posts/next-auth-postgres-adapter/
+  //     // let sql = postgres(process.env.PGSQL_HOST!, {
+  //     //   ssl: 'allow'
+  //     // });
+  //     const isAllowedToSignIn = true;
 
-      if (isAllowedToSignIn) {
-        // const { email, id, image, name } = user;
+  //     if (isAllowedToSignIn) {
+  //       // const { email, id, image, name } = user;
 
-        // if (!email || !id || !name) {
-        //   return false;
-        // }
+  //       // if (!email || !id || !name) {
+  //       //   return false;
+  //       // }
 
-        // const users = await sql`select * from user where twitch_id = id`;
+  //       // const users = await sql`select * from user where twitch_id = id`;
 
-        // if (users.length) {
-        //   const user = users[0] as User;
-        //   console.log('user', user);
-        // } else {
-        //   // first time signin, create user in db
-        //   const result =
-        //     await sql`insert into users (username, email, twitch_id) values(${name}, ${email}, ${id})`;
+  //       // if (users.length) {
+  //       //   const user = users[0] as User;
+  //       //   console.log('user', user);
+  //       // } else {
+  //       //   // first time signin, create user in db
+  //       //   const result =
+  //       //     await sql`insert into users (username, email, twitch_id) values(${name}, ${email}, ${id})`;
 
-        //   console.log('result', result);
-        // }
-        return true;
-      } else {
-        // Return false to display a default error message
-        return false;
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
-      }
-    }
-  }
+  //       //   console.log('result', result);
+  //       // }
+  //       return true;
+  //     } else {
+  //       // Return false to display a default error message
+  //       return false;
+  //       // Or you can return a URL to redirect to:
+  //       // return '/unauthorized'
+  //     }
+  //   }
+  // }
 });
