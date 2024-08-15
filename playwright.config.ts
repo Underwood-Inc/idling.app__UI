91f67e663f2f7fd8cd9408e41cd3ffcce9059041
underwood_inc <m.seaward@pm.me>
2024-08-15 11:45:43 -0300

    build: adjust output of playwright script

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'echo "building..."; yarn build; yarn start;',
+    command:
+      'echo "======================building..."; yarn build; yarn start; echo ""======================testing"',
     stdout: 'pipe',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI
