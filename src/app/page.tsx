11f2ace28d74237a783f898629a2629d9c40125c
Michael C. Seaward <m.seaward@mamok.in>
2024-08-16 07:36:29 -0300

    style: adjust colors to be WCAG contrast compliant (#98)
    Co-authored-by: underwood_inc <m.seaward@pm.me>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { About } from './components/about/About';
 import { Card } from './components/card/Card';
+import { PageContainer } from './components/page-container/PageContainer';
 import './globals.css';
 import styles from './page.module.css';
 
 export default async function Home() {
   return (
-    <>
+    <PageContainer>
       <article className={styles.home__container}>
         <Card width="min">
-          <h3>About</h3>
+          <h2>About</h2>
           <About />
         </Card>
       </article>
-    </>
+    </PageContainer>
   );
 }
