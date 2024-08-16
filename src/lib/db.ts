511a1bfd74736eee36b50882afbe17877cf0c124
underwood_inc <m.seaward@pm.me>
2024-08-16 13:54:25 -0300

    build: add github action postgres container

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import postgres from 'postgres';
 
 const sql = postgres({
-  host: process.env.PGSQL_HOST,
-  user: process.env.PGSQL_USER,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
   port: process.env.PGPORT as unknown as number,
   ssl: 'allow'
 });
