146d565ea46dbd9c2b66faedf74fd321d6dfd6ac
underwood <m.seaward@pm.me>
2024-08-19 16:18:03 -0300

    build: normalize env variables

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,10 +10,10 @@ export const {
   signOut
 } = NextAuth(() => {
   const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
 import postgres from 'postgres';
 
 const sql = postgres({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DB,
-  pass: process.env.PGPASSWORD,
+  pass: process.env.POSTGRES_PASSWORD,
   port: process.env.PGPORT as unknown as number,
   ssl: process.env.IS_CI ? 'prefer' : 'allow'
 });
