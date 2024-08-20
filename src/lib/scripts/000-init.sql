37604bb56b84d2ddf68b05dae0be82416126e699
Michael C. Seaward <m.seaward@mamok.in>
2024-08-20 18:35:28 -0300

    build: auto deploy script for new code merged into master (#113)
    * build: auto deploy script for new code merged into master
    
    ---------
    
    Co-authored-by: underwood <m.seaward@pm.me>

--- src/lib/scripts/000-init.sql
+++ src/lib/scripts/000-init.sql
@@ -1,6 +1,6 @@
-create database mydatabase with owner = postgres;
+create database idling with owner = postgres;
 
-\c mydatabase;
+\c idling;
 
 CREATE TABLE submissions (
   submission_id SERIAL NOT NULL PRIMARY KEY,
@@ -10,11 +10,57 @@ CREATE TABLE submissions (
   tags text[]
 );
 
-ALTER USER postgres WITH PASSWORD 'postgres';
-
 begin;
 set local timezone='UTC';
 alter table submissions alter column submission_datetime type timestamptz USING submission_datetime::timestamp with time zone;
 commit;
+
+CREATE TABLE verification_token
+(
+  identifier TEXT NOT NULL,
+  expires TIMESTAMPTZ NOT NULL,
+  token TEXT NOT NULL,
+ 
+  PRIMARY KEY (identifier, token)
+);
+ 
+CREATE TABLE accounts
+(
+  id SERIAL,
+  "userId" INTEGER NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  provider VARCHAR(255) NOT NULL,
+  "providerAccountId" VARCHAR(255) NOT NULL,
+  refresh_token TEXT,
+  access_token TEXT,
+  expires_at BIGINT,
+  id_token TEXT,
+  scope TEXT,
+  session_state TEXT,
+  token_type TEXT,
+ 
+  PRIMARY KEY (id)
+);
+ 
+CREATE TABLE sessions
+(
+  id SERIAL,
+  "userId" INTEGER NOT NULL,
+  expires TIMESTAMPTZ NOT NULL,
+  "sessionToken" VARCHAR(255) NOT NULL,
+ 
+  PRIMARY KEY (id)
+);
+ 
+CREATE TABLE users
+(
+  id SERIAL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  "emailVerified" TIMESTAMPTZ,
+  image TEXT,
+ 
+  PRIMARY KEY (id)
+);
 
 \q
