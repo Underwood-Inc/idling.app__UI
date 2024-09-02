fcc87bd0fa447d09d87bc14fba0ea1dde577a9cc
Michael C. Seaward <m.seaward@mamok.in>
2024-09-02 13:48:07 -0300

    Tests/components (#167)
    * test: add footernav component tests
    
    * test: add various component tests
    
    ---------
    
    Co-authored-by: underwood <m.seaward@pm.me>

--- src/app/components/game/GameWrapper.tsx
+++ src/app/components/game/GameWrapper.tsx
@@ -1,18 +1,27 @@
 'use client';
 import Script from 'next/script';
 import React from 'react';
+import { GAME_WRAPPER_SELECTORS } from 'src/lib/test-selectors/components/game-wrapper.selectors';
 import './styles.css';
 
 export const GameWrapper: React.FC = () => {
   return (
     <div>
       <div className="gm4html5_div_class" id="gm4html5_div_id">
-        <canvas id="canvas" width="864" height="648">
+        <canvas
+          id="canvas"
+          width="864"
+          height="648"
+          data-testid={GAME_WRAPPER_SELECTORS.CANVAS}
+        >
           <p>Your browser doesn&apos;t support HTML5 canvas.</p>
         </canvas>
       </div>
 
-      <Script src="idling.app.js?cachebust=968111397" />
+      <Script
+        data-testid={GAME_WRAPPER_SELECTORS.SCRIPT}
+        src="idling.app.js?cachebust=968111397"
+      />
     </div>
   );
 };
