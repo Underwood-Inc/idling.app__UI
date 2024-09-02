2f0a62628d0f1bddaa44fe04d721abe6ba18d92d
Michael C. Seaward <m.seaward@mamok.in>
2024-09-02 10:38:07 -0300

    test: add empty component tests (#160)
    Co-authored-by: underwood <m.seaward@pm.me>

--- src/app/components/empty/Empty.tsx
+++ src/app/components/empty/Empty.tsx
@@ -1,9 +1,10 @@
+import { EMPTY_SELECTORS } from 'src/lib/test-selectors/components/empty.selectors';
 import './Empty.css';
 
 export default function Empty({ label = '' }: { label: string }) {
   return (
-    <div className="empty">
-      <p>
+    <div className="empty" data-testid={EMPTY_SELECTORS.CONTAINER}>
+      <p data-testid={EMPTY_SELECTORS.LABEL}>
         {label && (
           <>
             {label}
---
+++ src/lib/test-selectors/components/empty.selectors.ts
@@ -0,0 +1,4 @@
+export const EMPTY_SELECTORS = {
+  CONTAINER: 'empty__container',
+  LABEL: 'empty__label'
+};
