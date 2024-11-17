920d178ad7f9c38598bb46709f5bc52a5790669b
Michael Seaward <m.seaward@pm.me>
2024-11-17 08:31:15 -0330

    feat: update about page to be more readable and responsive friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from './components/about/About';
 import { Card } from './components/card/Card';
 import { DiscordEmbed } from './components/discord-embed/DiscordEmbed';
+import { DiscordLink } from './components/discord-link/DiscordLink';
 import FadeIn from './components/fade-in/FadeIn';
 import FancyBorder from './components/fancy-border/FancyBorder';
 import { PageAside } from './components/page-aside/PageAside';
@@ -16,10 +17,22 @@ export default async function Home() {
         <PageContent>
           <article className={styles.home__container}>
             <FadeIn className={styles.home__container_fade}>
-              <Card width="min">
+              <Card className={styles.home__container_item}>
                 <h2>About</h2>
 
                 <About />
+              </Card>
+
+              <Card className={styles.home__container_item}>
+                <p>
+                  Join our <DiscordLink /> to get the latest news and
+                  occasionally view development via screen share events.
+                </p>
+                <p>
+                  See a bug? Report it in <DiscordLink /> and refer to the
+                  current site version located in the bottom right of the
+                  website footer.
+                </p>
               </Card>
             </FadeIn>
           </article>
