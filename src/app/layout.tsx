a967c871e2ce4c8da84a23005fad297be47fb48f
Michael C. Seaward <m.seaward@mamok.in>
2025-06-29 06:30:14 -0230

    Feat/google auto ads (#227)
    * fix(layout): add duplicate Google AdSense script for improved ad loading
    
    - Included an additional Google AdSense script tag in the layout to enhance ad integration.
    - Ensured the script is loaded asynchronously and maintains cross-origin settings for better performance.
    
    * chore: bump version to 0.267.0
    
    - Automatic version increment based on previous commit
    - This commit will not trigger further version bumps
    
    ---------
    
    Co-authored-by: underwood <m.seaward@pm.me>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -273,6 +273,12 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3757286003859686"
           crossOrigin="anonymous"
         ></script>
+
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3757286003859686"
+          crossOrigin="anonymous"
+        ></script>
       </head>
 
       <body className={inter.className}>
